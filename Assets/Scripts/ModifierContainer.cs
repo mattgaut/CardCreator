fa09c09028b478b8d900bf9ab8018634fc832a0b
matt_g <mgaut@uci.edu>
2018-08-21 11:55:25 -0700

    Created Modifier Container

---
+++ Assets/Scripts/ModifierContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Modifier { lifesteal = 1, charge, rush, divine_shield, immune, windfury, mega_windfury, poisonous, stealth, taunt, battlecry, overload }
+
+public class ModifierContainer : MonoBehaviour {
+
+    [SerializeField] protected List<Modifier> base_mods;
+    protected List<Modifier> buffed_mods;
+
+    [SerializeField] protected int _overload_cost;
+
+    public int overload_cost {
+        get { return _overload_cost; }
+    }
+
+    public bool HasMod(Modifier mod) {
+        return base_mods.Contains(mod) || buffed_mods.Contains(mod);
+    }
+    public void AddMod(Modifier mod) {
+        buffed_mods.Add(mod);
+    }
+    public void RemoveMod(Modifier mod) {
+        if (mod == Modifier.divine_shield || mod == Modifier.stealth) {
+            buffed_mods.RemoveAll(a => a == mod);
+            base_mods.RemoveAll(a => a == mod);
+        }
+        buffed_mods.Remove(mod);
+    }
+    public void RemoveAllMods() {
+        buffed_mods.Clear();
+        base_mods.Clear();
+    }
+
+    protected virtual void OnAwake() {
+    }
+
+    private void Awake() {
+        buffed_mods = new List<Modifier>();
+        OnAwake();
+    }
+}
