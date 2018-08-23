d51df7e0f068b17db7a7f0ca903c1ad657069dbc
matt_g <mgaut@uci.edu>
2018-08-23 01:06:55 -0700

    Added name display

--- Assets/Scripts/CardScripts/CardDisplayScripts/FullCardDisplay.cs
+++ Assets/Scripts/CardScripts/CardDisplayScripts/FullCardDisplay.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class FullCardDisplay : CardDisplay {
 
-    [SerializeField] protected Text mana_cost;
+    [SerializeField] protected Text mana_cost, card_name;
 
     public override void UpdateDisplay() {
         mana_cost.text = "" + card.mana_cost;
+        card_name.text = card.name;
     }
 }
