8e60d3d9bfafb27018793209608a13e62920c29c
matt_g <mgaut@uci.edu>
2018-08-24 23:17:10 -0700

    Created Freeze Target Effect

---
+++ Assets/Scripts/Effects/Targeted/FreezeTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeTarget : TargetedEffect {
+
+    protected override void Resolve(Card source, IEntity target) {
+        ICombatant combatant = target as ICombatant;
+        if (combatant != null) {
+            combatant.Freeze();
+        }
+    }
+}
