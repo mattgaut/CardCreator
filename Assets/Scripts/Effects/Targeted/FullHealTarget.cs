4b9f5e19d40aa91abc6f27a7d2a65dbcc055fb31
matt_g <mgaut@uci.edu>
2018-08-24 22:11:54 -0700

    Created FullHealTarget Effect

--- Assets/Scripts/Effects/Targeted/FullHealTarget.cs
+++ Assets/Scripts/Effects/Targeted/FullHealTarget.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealTarget : TargetedEffect {
-    [SerializeField] int heal_amount;
-
+public class FullHealTarget : TargetedEffect {
     protected override void Resolve(Card source, IEntity target) {
         IHealth to_heal = target as IHealth;
         if (to_heal != null) {
-            to_heal.Heal(source, heal_amount);
+            to_heal.Heal(source, to_heal.max_health - to_heal.current_health);
         }
     }
 }
