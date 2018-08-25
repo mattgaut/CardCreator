ecca47cd53719a7436e1731b7e593eb46100e21f
matt_g <mgaut@uci.edu>
2018-08-24 23:05:54 -0700

    Created Destroy Target Effect

---
+++ Assets/Scripts/Effects/Targeted/DestroyTarget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTarget : TargetedEffect {
+    protected override void Resolve(Card source, IEntity target) {
+        Creature creature = target as Creature;
+        if (creature != null) {
+            creature.Destroy();
+        }
+    }
+}
