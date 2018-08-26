2e27cc452cf706a1b17056e31b34a715854202f6
matt_g <mgaut@uci.edu>
2018-08-25 19:22:38 -0700

    Created Stat Class

---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat {
+
+    [SerializeField] int _base_value;
+
+    public int value { get { return base_value + buff_value; } }
+    public int base_value { get { return use_force_value ? force_value : base_value + buff_value; } }
+
+    int force_value;
+    bool use_force_value;
+
+    int buff_value;
+
+    public void ApplyBuff(int buff_value) {
+        this.buff_value += buff_value;
+    }
+
+    public void RemoveBuff(int buff_value) {
+        this.buff_value -= buff_value;
+    }
+
+    public void SetValue(int value_to_set) {
+        buff_value = value_to_set - base_value;
+    }
+
+    public void ForceValue(int force_value) {
+        this.force_value = force_value;
+        use_force_value = true;
+    }
+
+    public void ClearForceValue() {
+        force_value = 0;
+        use_force_value = false;
+    }
+
+}
