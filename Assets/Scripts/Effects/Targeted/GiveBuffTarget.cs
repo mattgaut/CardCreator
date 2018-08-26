0affecd226fa5dd3733ff3365dd9a07d943c5795
matt_g <mgaut@uci.edu>
2018-08-26 10:23:28 -0700

    Added targeted give buff effect

---
+++ Assets/Scripts/Effects/Targeted/GiveBuffTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveBuffTarget : TargetedEffect {
+
+    [SerializeField] int health_buff_amount;
+    [SerializeField] int attack_buff_amount;
+
+    protected override void Resolve(Card source, IEntity target) {
+        Creature creature = target as Creature;
+        if (creature != null) {
+            creature.attack.ApplyBuff(attack_buff_amount);
+            creature.max_health.ApplyBuff(health_buff_amount);
+        }
+    }
+}
