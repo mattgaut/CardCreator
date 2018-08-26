805120fd5a8f833aada75f0879aa0c5deaa0589e
matt_g <mgaut@uci.edu>
2018-08-26 11:17:53 -0700

    Created Add Token To Hand Class

---
+++ Assets/Scripts/Effects/Untargeted/AddTokensToHand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTokensToHand : UntargetedEffect {
+
+    [SerializeField] List<Card> to_add;
+
+    public override void Resolve(Card source) {
+        foreach (Card card in to_add) {
+            GameStateManager.instance.CreateToken(source.controller.hand, card);
+        }
+    }
+}
