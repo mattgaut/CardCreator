e2adaed2da869bb5094bbe1bc79eb12c90b19d9b
matt_g <mgaut@uci.edu>
2018-08-26 10:40:19 -0700

    Created GiveMassBuff effect

---
+++ Assets/Scripts/Effects/Untargeted/GiveMassBuff.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GiveMassBuff : UntargetedEffect {
+
+    [SerializeField] int health_buff_amount, attack_buff_amount;
+    [SerializeField] bool friendly, enemy;
+
+    public override void Resolve(Card source) {
+        List<Creature> creatures_to_affect = new List<Creature>();
+        if (friendly) {
+            creatures_to_affect.AddRange(source.controller.field.cards.OfType<Creature>());
+        }
+        if (enemy) {
+            foreach (Player p in GameManager.players) {
+                if (p == source.controller) continue;
+
+                creatures_to_affect.AddRange(p.field.cards.OfType<Creature>());
+            }
+        }
+
+        foreach (Creature c in creatures_to_affect) {
+            c.attack.ApplyBuff(attack_buff_amount);
+            c.max_health.ApplyBuff(health_buff_amount);
+        }
+    }
+}
