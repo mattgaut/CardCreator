5cdf248b3d70bcaa1bddf3e2f180fa2b25a96114
matt_g <mgaut@uci.edu>
2018-08-26 10:54:01 -0700

    Created reducemanacostinhand effect

---
+++ Assets/Scripts/Effects/Untargeted/ReduceManaCostInHand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReduceManaCostInHand : UntargetedEffect {
+
+    [SerializeField] int to_reduce;
+
+    public override void Resolve(Card source) {
+        foreach (Card c in source.controller.hand.cards) {
+            c.mana_cost.ApplyBuff(to_reduce);
+        }
+    }
+}
