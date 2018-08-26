3aaed32e86184a7a01d9f23cff396b8d77d9538d
matt_g <mgaut@uci.edu>
2018-08-26 11:19:16 -0700

    Created Add Token To Field Effect

---
+++ Assets/Scripts/Effects/Untargeted/AddTokenToField.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTokenToField : UntargetedEffect {
+
+    [SerializeField] List<Creature> to_add;
+
+    public override void Resolve(Card source) {
+        foreach (Card card in to_add) {
+            GameStateManager.instance.CreateToken(source.controller.field, card);
+        }
+    }
+}
