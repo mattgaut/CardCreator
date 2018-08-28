236bc532de7143a23bbdcd31fad4956c4fad7ee3
matt_g <mgaut@uci.edu>
2018-08-27 17:59:19 -0700

    Updated To hold new trigger types

--- Assets/Scripts/AbilityHolder.cs
+++ Assets/Scripts/AbilityHolder.cs
@@ -5,8 +5,14 @@ using System.Linq;
 
 [RequireComponent(typeof(Card))]
 public class AbilityHolder : MonoBehaviour {
-    [SerializeField] List<TriggeredAbility> global_triggered_abilities;
-    [SerializeField] List<TriggeredAbility> local_triggered_abilities;
+   
+    [SerializeField] List<ETBTriggeredAbility> etb_triggered_abilities;
+    [SerializeField] List<AfterAttackTriggeredAbility> after_attack_triggered_abilities;
+    [SerializeField] List<BeforeSpellTriggeredAbility> before_spell_triggered_abilities;
+    [SerializeField] List<AfterSpellTriggeredAbility> after_spell_triggered_abilities;
+
+    List<TriggeredAbility> global_triggered_abilities;
+    List<TriggeredAbility> local_triggered_abilities;
 
     public Card card {
         get; private set;
@@ -15,10 +21,13 @@ public class AbilityHolder : MonoBehaviour {
     private void Awake() {
         card = GetComponent<Card>();
         global_triggered_abilities = new List<TriggeredAbility>();
-        global_triggered_abilities.AddRange(global_triggered_abilities);
-        foreach (TriggeredAbility ta in global_triggered_abilities) {
-            ta.SetSource(card);
-        }
+        local_triggered_abilities = new List<TriggeredAbility>();
+
+        // Add All trigger types to their proper lists
+        AddTriggersToList(etb_triggered_abilities.OfType<TriggeredAbility>());
+        AddTriggersToList(after_attack_triggered_abilities.OfType<TriggeredAbility>());
+        AddTriggersToList(before_spell_triggered_abilities.OfType<TriggeredAbility>());
+        AddTriggersToList(after_spell_triggered_abilities.OfType<TriggeredAbility>());
     }
 
     public IEnumerable<TriggeredAbility> GetTriggersActiveInZone(Zone zone) {
@@ -29,6 +38,15 @@ public class AbilityHolder : MonoBehaviour {
         global_triggered_abilities.Add(ta);
         ta.SetSource(card);
     }
-}
 
-public enum AbilityType { constant, trigger, none }
+    void AddTriggersToList(IEnumerable<TriggeredAbility> abilities) {
+        foreach (TriggeredAbility ta in abilities) {
+            if (ta.is_local) {
+                local_triggered_abilities.Add(ta);
+            } else {
+                global_triggered_abilities.Add(ta);
+            }
+            ta.SetSource(card);
+        }
+    }
+}
