bf00d8a9c24f7eb6b61fd5694f1901f3729506b8
matt_g <mgaut@uci.edu>
2018-08-27 17:24:01 -0700

    Created AfterSpellTriggeredAbility

---
+++ Assets/Scripts/Triggers/AfterSpellTriggeredAbility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterSpellTriggeredAbility : TriggeredAbility {
+
+    [SerializeField] CompareEntity compare;
+
+    public override TriggerType type {
+        get {
+            return TriggerType.after_spell_resolves;
+        }
+    }
+
+    public override bool TriggersFrom(TriggerInfo info) {
+        if (info.type != TriggerType.after_spell_resolves) {
+            return false;
+        }
+        AfterSpellTriggerInfo after_spell_info = (AfterSpellTriggerInfo)info;
+
+        return compare.CompareTo(after_spell_info.spell_cast, source);
+    }
+}
+
+public class AfterSpellTriggerInfo : TriggerInfo {
+    public override TriggerType type {
+        get {
+            return TriggerType.after_spell_resolves;
+        }
+    }
+
+    public Spell spell_cast { get; private set; }
+    
+    public AfterSpellTriggerInfo(Spell cast) {
+        spell_cast = cast;
+    }
+
+}
