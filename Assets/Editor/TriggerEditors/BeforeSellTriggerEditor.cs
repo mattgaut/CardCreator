28cba075a96b621e1edd3a849295237a14315cea
matt_g <mgaut@uci.edu>
2018-08-28 17:49:37 -0700

    Created Before Spell Trigger Editor

---
+++ Assets/Editor/TriggerEditors/BeforeSellTriggerEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(BeforeSpellTriggeredAbility))]
+public class BeforeSpellTriggerEditor : TriggerEditor {
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginProperty(position, label, property);
+
+            property.isExpanded = EditorGUILayout.Foldout(property.isExpanded, "Before Spell Trigger");
+            if (property.isExpanded) {
+                EditorGUI.indentLevel += 1;
+
+                BaseTriggerGUI(property);
+
+                SerializedProperty child_property = property.FindPropertyRelative("compare");
+                EditorGUILayout.PropertyField(child_property, new GUIContent("Matching"));
+
+                EditorGUI.indentLevel -= 1;
+            }
+            EditorGUI.EndProperty();
+        }
+    }
