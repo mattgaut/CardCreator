48ed543f4dbabdb963ca1fe418c596640d9871f0
matt_g <mgaut@uci.edu>
2018-08-29 19:38:23 -0700

    Created Static Ability

---
+++ Assets/Scripts/Statics/StaticAbility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StaticAbility : MonoBehaviour {
+
+    [SerializeField] List<Zone> active_zones;
+
+    [SerializeField] List<Zone> affected_zones;
+    [SerializeField] bool affects_players;
+
+    public Card source { get; private set; }
+
+    public virtual bool AppliesTo(IEntity entity) {
+        return true;
+    }
+
+    public abstract void Apply(IEntity apply_to);
+    public abstract void Remove(IEntity remove_from);
+}
