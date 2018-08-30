e451bab865e23f25de3a310aeca97c09ca28ea86
matt_g <mgaut@uci.edu>
2018-08-29 19:50:28 -0700

    Updated Static Ability

--- Assets/Scripts/Statics/StaticAbility.cs
+++ Assets/Scripts/Statics/StaticAbility.cs
@@ -9,12 +9,41 @@ public abstract class StaticAbility : MonoBehaviour {
     [SerializeField] List<Zone> affected_zones;
     [SerializeField] bool affects_players;
 
+    HashSet<IEntity> entities_applied_to;
+
     public Card source { get; private set; }
+
+    public List<Zone> GetActiveZones() {
+        return new List<Zone>(active_zones);
+    }
+
+    public List<Zone> GetAffectedZones() {
+        return new List<Zone>(affected_zones);
+    }
 
     public virtual bool AppliesTo(IEntity entity) {
         return true;
     }
 
-    public abstract void Apply(IEntity apply_to);
-    public abstract void Remove(IEntity remove_from);
+    public bool IsAppliedTo(IEntity entity) {
+        return entities_applied_to.Contains(entity);
+    }
+
+    public void Apply(IEntity apply_to) {
+        if (!entities_applied_to.Contains(apply_to)) {
+            ApplyEffects(apply_to);
+        }
+    }
+    public void Remove(IEntity remove_from) {
+        if (entities_applied_to.Contains(remove_from)) {
+            RemoveEffects(remove_from);
+        }
+    }
+
+    protected abstract void ApplyEffects(IEntity apply_to);
+    protected abstract void RemoveEffects(IEntity remove_from);
+
+    private void Awake() {
+        entities_applied_to = new HashSet<IEntity>();    
+    }
 }
