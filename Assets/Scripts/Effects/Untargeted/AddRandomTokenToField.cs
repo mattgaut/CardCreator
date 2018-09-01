313b3b5773b336db04ae0428468e99d020f33ebf
matt_g <mgaut@uci.edu>
2018-09-01 02:50:19 -0700

    Created Add RandomTokenToField Effect

---
+++ Assets/Scripts/Effects/Untargeted/AddRandomTokenToField.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRandomTokenToField : UntargetedEffect {
+
+    [SerializeField] List<Card> possible_cards;
+
+    public override void Resolve(Card source) {
+        GameStateManager.instance.CreateToken(source.controller.field, possible_cards[Random.Range(0, possible_cards.Count)]);
+    }
+}
