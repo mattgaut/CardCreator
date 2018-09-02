2fa7000c9922678a2cc1ec100ffa7d2c56663eb0
matt_g <mgaut@uci.edu>
2018-09-02 01:57:54 -0700

    Created WeaponFieldDisplay

---
+++ Assets/Scripts/CardScripts/CardDisplayScripts/WeaponFieldDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Weapon))]
+public class WeaponFieldDisplay : CardDisplay {
+    Weapon weapon;
+
+    [SerializeField] Text attack, durability;
+
+    public override void UpdateDisplay() {
+        attack.text = "" + weapon.attack;
+        durability.text = "" + weapon.durability.current_value;
+    }
+
+    protected override void Awake() {
+        base.Awake();
+        weapon = GetComponent<Weapon>();
+    }
+}
