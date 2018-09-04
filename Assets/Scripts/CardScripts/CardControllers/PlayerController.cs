535d27f1d51e79b485efc097adf6e7c4dd91a02b
matt_g <mgaut@uci.edu>
2018-09-03 22:02:06 -0700

    Created Player Controller

---
+++ Assets/Scripts/CardScripts/CardControllers/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IClickable {
+
+    Player player;
+
+    public void OnClick() {
+    }
+
+    public void OnEndClick() {
+        InterfaceManager.RemoveTargetingArrow();
+    }
+
+    public void OnFinishDrag(GameObject dragged_to, Vector3 position_dragged_to) {
+        if (player.can_attack) {
+            ICombatant target;
+            if ((target = dragged_to.GetComponent<ICombatant>()) == null) {
+                return;
+            }
+            player.command_manager.AddCommand(new AttackCommand(target, player));
+        }
+    }
+
+    public void OnHoldDrag(GameObject dragged_to, Vector3 position_dragged_to) {
+        if (player.can_attack) {
+            InterfaceManager.DrawTargetingArrow(transform.position, position_dragged_to);
+        }
+    }
+
+    public void OnMouseDown() {
+        
+    }
+}
