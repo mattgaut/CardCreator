710e5ae7e446ee3783ff67ba8e09916f6ce235ae
matt_g <mgaut@uci.edu>
2018-09-04 03:10:52 -0700

    Created Secret Class

---
+++ Assets/Scripts/CardScripts/Secret.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secret : Card {
+
+    public override CardType type {
+        get {
+            return CardType.Spell;
+        }
+    }
+
+    public override void Resolve() {
+    }
+
+    public override int DealDamage(IDamageable target, int damage) {
+        return base.DealDamage(target, damage + controller.TotalSpellPower());
+    }
+
+}
