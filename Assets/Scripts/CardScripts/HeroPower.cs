de80d7e7371d0c7ba2e0bbd89ccd36a9b7ae6fdc
matt_g <mgaut@uci.edu>
2018-09-10 04:42:59 -0700

    Added HeroPower

---
+++ Assets/Scripts/CardScripts/HeroPower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroPower : MonoBehaviour, IDamages, IStackEffect {
+
+    [SerializeField] Stat _mana_cost;
+
+    [SerializeField] List<UntargetedEffect> untargeted_effects;
+
+    public Player controller { get; private set; }
+
+    public EntityType entity_type {
+        get { return EntityType.hero_power; }
+    }
+
+    public bool CanBeTargeted(IEntity source) {
+        return true;
+    }
+
+    public int DealDamage(IDamageable target, int damage) {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void Resolve() {
+        for (int i = 0; i < untargeted_effects.Count; i++) {
+            untargeted_effects[i].Resolve(this);
+        }
+    }
+}
