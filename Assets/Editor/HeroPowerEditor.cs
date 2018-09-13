2aa7c388c90ca6abc5a29dea6f3d21b6d806044d
matt_g <mgaut@uci.edu>
2018-09-13 09:35:56 -0700

    Created HeroPowerEditor

---
+++ Assets/Editor/HeroPowerEditor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(HeroPower))]
+public class HeroPowerEditor : Editor {
+    // Class Only exists to allow CompareEntity classes to be drawn properly on hero power
+}
