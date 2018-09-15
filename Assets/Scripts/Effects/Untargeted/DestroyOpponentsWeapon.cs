e4f11d64d54353d1500cca8fb7d5abe90c157274
matt_g <mgaut@uci.edu>
2018-09-15 16:16:57 -0700

    Created Destroy Opponents Weapon script

---
+++ Assets/Scripts/Effects/Untargeted/DestroyOpponentsWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOpponentsWeapon : UntargetedEffect {
+    public override void Resolve(IEntity source) {
+        foreach (Player player in GameManager.players) {
+            if (player == source.controller) continue;
+
+            if (player.weapon != null) {
+                player.weapon.Destroy();
+            }
+        }
+    }
+}
