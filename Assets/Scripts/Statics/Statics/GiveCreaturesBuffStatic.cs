c643be939033e3265762c8bb3b10a48063d49f70
matt_g <mgaut@uci.edu>
2018-09-18 12:37:51 -0700

    Created GiveCreaturesBuffStatic

---
+++ Assets/Scripts/Statics/Statics/GiveCreaturesBuffStatic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveCreaturesBuffStatic : StaticAbility {
+
+    [SerializeField] int attack_buff;
+    [SerializeField] int health_buff;
+
+    protected override void ApplyEffects(IEntity apply_to) {
+        Creature creature = apply_to as Creature;
+        if (creature != null) {
+            if (attack_buff != 0) creature.attack.ApplyBuff(attack_buff);
+            if (health_buff != 0) creature.health.ApplyBuff(health_buff);
+        }
+    }
+
+    protected override void RemoveEffects(IEntity remove_from) {
+        Creature creature = remove_from as Creature;
+        if (creature != null) {
+            
+            if (attack_buff != 0) creature.attack.RemoveBuff(attack_buff);
+            if (health_buff != 0) creature.health.RemoveBuff(health_buff);
+        }
+    }
+}
