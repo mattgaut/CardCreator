583afab43bcaf17f8675a61407cb35e751ada061
matt_g <mgaut@uci.edu>
2018-09-19 13:08:24 -0700

    Made AttackRestriction not abstract

--- Assets/Scripts/AttackRestriction.cs
+++ Assets/Scripts/AttackRestriction.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AttackRestriction : MonoBehaviour {
+public class AttackRestriction : MonoBehaviour {
 
     Func<ICombatant, bool> function;
 
