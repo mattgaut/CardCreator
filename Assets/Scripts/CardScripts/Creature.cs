8c6a21681626f400239a2c781075078b6a8a6846
matt_g <mgaut@uci.edu>
2018-09-18 19:20:50 -0700

    Added Creature Type

--- Assets/Scripts/CardScripts/Creature.cs
+++ Assets/Scripts/CardScripts/Creature.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Creature : Card, ICombatant {
 
-    public enum CreatureType { none, beast, mech, dragon }
+    public enum CreatureType { none, beast, mech, dragon, murloc }
 
     [SerializeField] Stat _attack;
     [SerializeField] ResourceStat _health;
