21245c4ccee70b79ee65fae6df82c2e23a619bd0
matt_g <mgaut@uci.edu>
2018-09-19 13:37:53 -0700

    Created Effect to give player attack buff

---
+++ Assets/Scripts/Effects/Untargeted/GivePlayerTimedAttackBuff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GivePlayerTimedAttackBuff : UntargetedEffect, ITimedEffect {
+    [SerializeField] TimePoint _time_point;
+    [SerializeField] int attack_buff;
+
+    public TimePoint time_point {
+        get { return _time_point; }
+    }
+
+    public void EndEffect(IEntity source, IEntity target = null) {
+        if (target != null) {
+            return;
+        }
+        Player player = source.controller;
+
+        if (player != null) {
+            player.attack.RemoveBuff(attack_buff);
+        }
+    }
+
+    public override void Resolve(IEntity source) {
+        Player player = source.controller;
+
+        if (player != null) {
+            player.attack.ApplyBuff(attack_buff);
+            
+            GameStateManager.instance.TrackTimedEffect(this, source);
+        }
+    }
+}
