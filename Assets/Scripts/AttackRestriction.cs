28da30a362800f2bf94a4aa20610be3159106f4f
matt_g <mgaut@uci.edu>
2018-09-19 13:22:30 -0700

    Fixed Attack Restriction To not Derive from monobehaviour

--- Assets/Scripts/AttackRestriction.cs
+++ Assets/Scripts/AttackRestriction.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackRestriction : MonoBehaviour {
+public class AttackRestriction {
 
     Func<ICombatant, bool> function;
 
