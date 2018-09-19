8c4cdea3dcbc961887b2b525ab78f3e1874c1aa4
matt_g <mgaut@uci.edu>
2018-09-19 13:38:04 -0700

    Created Effect To give player armor

---
+++ Assets/Scripts/Effects/Untargeted/PlayerGainArmor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGainArmor : UntargetedEffect {
+
+    [SerializeField] int to_gain;
+
+    public override void Resolve(IEntity source) {
+        source.controller.GainArmor(source, to_gain);
+    }
+
+}
