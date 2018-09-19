5e77c706c4f48e5c2dee832b937212f96f4f9799
matt_g <mgaut@uci.edu>
2018-09-19 14:53:10 -0700

    Created Destroy Minion After Time effect

---
+++ Assets/Scripts/Effects/Targeted/DestroyMinionAfterTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMinionAfterTime : TargetedEffect, ITimedEffect {
+    [SerializeField] TimePoint _time_point;
+
+    public TimePoint time_point {
+        get { return _time_point; }
+    }
+
+    public void EndEffect(IEntity source, IEntity target = null) {
+        if (target as Creature == null) {
+            return;
+        }
+        (target as Creature).Destroy();
+    }
+
+    protected override void Resolve(IEntity source, IEntity target) {
+        if (target as Creature == null) {
+            return;
+        }
+        GameStateManager.instance.TrackTimedEffect(this, source, target);
+    }
+}
