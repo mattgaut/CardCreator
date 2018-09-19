85b9167e66a610c2b7a56ac4bc66dbecf2c2ab83
matt_g <mgaut@uci.edu>
2018-09-18 19:17:35 -0700

    Created Mass Timed Buff Effect

---
+++ Assets/Scripts/Effects/Untargeted/GiveMassTimedBuff.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GiveMassTimedBuff : UntargetedEffect, ITimedEffect {
+
+    [SerializeField] TimePoint lasts_until;
+
+    [SerializeField] bool friendly, enemy;
+    [SerializeField] int attack_buff, health_buff;
+
+    public TimePoint time_point {
+        get { return lasts_until; }
+    }
+
+    public void EndEffect(IEntity source, IEntity target = null) {
+        if (target == null) {
+            return;
+        }
+
+        Creature creature = target as Creature;
+        if (creature == null) {
+            return;
+        }
+
+        creature.attack.RemoveBuff(attack_buff);
+        creature.health.RemoveBuff(health_buff);
+    }
+
+    public override void Resolve(IEntity source) {
+        if (friendly) {
+            foreach (Creature creature in source.controller.field.cards.OfType<Creature>()) {
+                creature.attack.ApplyBuff(attack_buff);
+                creature.health.ApplyBuff(health_buff);
+            }
+        }
+        if (enemy) {
+            foreach (Player player in GameManager.players) {
+                if (player == source.controller) {
+                    continue;
+                }
+                foreach (Creature creature in player.field.cards.OfType<Creature>()) {
+                    creature.attack.ApplyBuff(attack_buff);
+                    creature.health.ApplyBuff(health_buff);
+                }
+            }            
+        }
+    }
+}
