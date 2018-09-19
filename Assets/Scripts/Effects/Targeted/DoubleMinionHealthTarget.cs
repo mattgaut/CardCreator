41c4eb56124247920d597ce857a68d6f0088c095
matt_g <mgaut@uci.edu>
2018-09-19 15:16:20 -0700

    Created Double Minion Health Effect

---
+++ Assets/Scripts/Effects/Targeted/DoubleMinionHealthTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleMinionHealthTarget : TargetedEffect {
+
+    protected override void Resolve(IEntity source, IEntity target) {
+        Creature creature = target as Creature;
+        if (creature == null) {
+            return;
+        }
+
+        creature.health.ApplyBuff(creature.current_health);
+    }
+}
