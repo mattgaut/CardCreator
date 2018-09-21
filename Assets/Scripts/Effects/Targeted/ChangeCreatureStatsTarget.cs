3167aece39570dc8f548235636dbdaa63f077003
matt_g <mgaut@uci.edu>
2018-09-21 15:47:30 -0700

    Created ChangeCreatureStatsTarget effect

---
+++ Assets/Scripts/Effects/Targeted/ChangeCreatureStatsTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCreatureStatsTarget : TargetedEffect {
+
+    [SerializeField] bool change_attack;
+    [SerializeField] int new_attack;
+    [SerializeField] bool change_health;
+    [SerializeField] int new_health;
+
+    protected override void Resolve(IEntity source, IEntity target) {
+        Creature creature = target as Creature;
+        if (creature == null) {
+            return;
+        }
+
+        if (change_attack) creature.attack.AddSetBuff(new StatBuff(source, StatBuff.Type.basic, new_attack));
+        if (change_health) creature.attack.AddSetBuff(new StatBuff(source, StatBuff.Type.basic, new_health));
+    }
+}
