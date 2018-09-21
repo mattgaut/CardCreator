4f0c8ddc28c2c2a381d5c69c0f061e5feeb14a97
matt_g <mgaut@uci.edu>
2018-09-21 13:18:12 -0700

    Created TransformMinionTarget effect

---
+++ Assets/Scripts/Effects/Targeted/TransformMinionTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMinionTarget : TargetedEffect {
+
+    [SerializeField] Creature transform_into;
+
+    protected override void Resolve(IEntity source, IEntity target) {
+        Creature creature = target as Creature;
+        if (creature == null) {
+            return;
+        }
+
+        GameStateManager.instance.TransformCreature(creature, transform_into);
+    }
+}
