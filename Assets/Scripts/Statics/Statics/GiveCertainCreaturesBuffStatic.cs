2ea5b89e510fa4007e83c41241ca1211a484dff3
matt_g <mgaut@uci.edu>
2018-09-21 00:18:33 -0700

    Created new static effect

---
+++ Assets/Scripts/Statics/Statics/GiveCertainCreaturesBuffStatic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveCertainCreaturesBuffStatic : GiveCreaturesBuffStatic {
+
+    [SerializeField] CompareEntity type_to_buff;
+
+    public override bool AppliesTo(IEntity entity) {
+
+        if (!type_to_buff.CompareTo(entity, source)) {
+            return false;
+        }
+
+        return base.AppliesTo(entity);
+    }
+}
