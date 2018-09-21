7908768c3af96d9d11359d12eef23273d1c59738
matt_g <mgaut@uci.edu>
2018-09-20 20:14:48 -0700

    Created Gain Buff for each creature on the field

---
+++ Assets/Scripts/Effects/Untargeted/GainBuffForEachCreatureOnTheField.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GainBuffForEachCreatureOnTheField : UntargetedEffect {
+
+    [SerializeField] int attack_buff, health_buff;
+    [SerializeField] bool friendly, enemy;
+
+    public override void Resolve(IEntity source) {
+        int count = 0;
+        if (friendly) {
+            count += source.controller.field.count;
+        }
+        if (enemy) {
+            foreach (Player player in GameManager.OtherPlayers(source.controller)) {
+                count += player.field.count;
+            }
+        }
+
+        Creature creature = source as Creature;
+
+        if (creature) {
+            if (attack_buff != 0) {
+                creature.attack.ApplyBuff(count * attack_buff);
+            }
+            if (health_buff != 0) {
+                creature.health.ApplyBuff(count * health_buff);
+            }
+        }
+    }
+}
