2dc89d5c844ecc8a6a5909af2911429c168976a8
matt_g <mgaut@uci.edu>
2018-09-20 17:51:59 -0700

    Created Adjacent Creature buff static effects

---
+++ Assets/Scripts/Statics/GiveAdjacentCreaturesBuffStatic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GiveAdjacentCreaturesBuff : StaticAbility {
+
+    public override bool AppliesTo(IEntity entity) {
+        if (source.type != CardType.Creature) {
+            return false;
+        }
+
+        Card card = entity as Card;
+        if (card == null) {
+            return false;
+        }
+
+        if (card.container != source.container) {
+            return false;
+        }
+        int source_position = source.container.CardPosition(source);
+        int card_position = card.container.CardPosition(card);
+
+        if (System.Math.Abs(card_position - source_position) != 1) {
+            return false;
+        }
+
+        return base.AppliesTo(entity);
+    }
+
+}
---
+++ Assets/Scripts/Statics/Statics/GiveAdjacentCreaturesAttackStatic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveAdjacentCreaturesAttackStatic : GiveAdjacentCreaturesBuff {
+    [SerializeField] int attack_buff;
+
+    protected override void ApplyEffects(IEntity apply_to) {
+        Creature creature = apply_to as Creature;
+        if (creature == null) {
+            return;
+        }
+        creature.attack.ApplyBuff(attack_buff);
+    }
+
+    protected override void RemoveEffects(IEntity remove_from) {
+        Creature creature = remove_from as Creature;
+        if (creature == null) {
+            return;
+        }
+        creature.attack.RemoveBuff(attack_buff);
+    }
+}
