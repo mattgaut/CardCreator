0800fc2f5a34b4f5811bd69f214a204528dfa0dc
matt_g <mgaut@uci.edu>
2018-09-21 12:04:06 -0700

    Created Give Buff Effect

---
+++ Assets/Scripts/Effects/Untargeted/GiveBuff.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveBuff : UntargetedEffect {
+
+    [SerializeField] int health_buff_amount, attack_buff_amount;
+
+    public override void Resolve(IEntity source) {
+        Creature c = source as Creature;
+
+        if (attack_buff_amount != 0) c.attack.ApplyBuff(attack_buff_amount);
+        if (health_buff_amount != 0) c.health.ApplyBuff(health_buff_amount);
+    }
+}
