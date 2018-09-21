465f8210f15d2304ebebc30bf43016ce759f4f37
matt_g <mgaut@uci.edu>
2018-09-20 18:06:53 -0700

    Created Mass Freeze Effect

---
+++ Assets/Scripts/Effects/Untargeted/MassFreeze.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MassFreeze : UntargetedEffect {
+
+    [SerializeField] bool friendly, enemy;
+    [SerializeField] bool affects_players;
+
+    public override void Resolve(IEntity source) {
+        if (affects_players) {
+            if (friendly) {
+                source.controller.Freeze();
+            }
+            if (enemy) {
+                foreach (Player player in GameManager.players) {
+                    if (player == source.controller) {
+                        continue;
+                    }
+                    player.Freeze();
+                }
+            }
+        }
+
+        if (friendly) {
+            foreach (Creature creature in source.controller.field.cards.OfType<Creature>()) {
+                creature.Freeze();
+            }
+        }
+        
+        if (enemy) {
+            foreach (Player player in GameManager.players) {
+                if (player == source.controller) {
+                    continue;
+                }
+                foreach (Creature creature in player.field.cards.OfType<Creature>()) {
+                    creature.Freeze();
+                }
+            }
+        }
+    }
+}
