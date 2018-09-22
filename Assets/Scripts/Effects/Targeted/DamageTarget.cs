dd557faf522645b25c6572d4f8b0d5580501d4b0
matt_g <mgaut@uci.edu>
2018-09-21 20:13:01 -0700

    Created DamageTargetBonusWithCreature effect

--- Assets/Scripts/Effects/Targeted/DamageTarget.cs
+++ Assets/Scripts/Effects/Targeted/DamageTarget.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageTarget : TargetedEffect {
-    [SerializeField] int damage;
+    [SerializeField] protected int damage;
     protected override void Resolve(IEntity source, IEntity target) {
         IDamageable damageable = target as IDamageable;
         if (damageable != null) {
---
+++ Assets/Scripts/Effects/Targeted/DamageTargetBonusWithCreature.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTargetBonusWithCreature : DamageTarget {
+
+    [SerializeField] int bonus_damage;
+    [SerializeField] Creature.CreatureType creature_type;
+
+    protected override void Resolve(IEntity source, IEntity target) {
+        bool has_creature = false;
+
+        foreach (Creature creature in source.controller.field.cards) {
+            if (creature.creature_type == creature_type) {
+                has_creature = true;
+                break;
+            }
+        }
+
+        if (has_creature) {
+            IDamageable damageable = target as IDamageable;
+            if (damageable != null) {
+                source.DealDamage(damageable, damage + bonus_damage);
+            }
+        } else {
+            base.Resolve(source, target);
+        }
+    }
+}
