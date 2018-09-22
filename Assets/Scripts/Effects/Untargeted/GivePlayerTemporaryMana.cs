0f2570df8f71371c5f34fed87e52abcc4a55fee8
matt_g <mgaut@uci.edu>
2018-09-21 18:24:46 -0700

    Created GivePlayerTemporaryMana effect

---
+++ Assets/Scripts/Effects/Untargeted/GivePlayerTemporaryMana.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GivePlayerTemporaryMana : UntargetedEffect {
+
+    [SerializeField] int amount;
+
+    public override void Resolve(IEntity source) {
+        source.controller.GainMana(amount, true);
+    }
+}
