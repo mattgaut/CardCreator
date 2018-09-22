5af511cdfc82684179f20a76bc6dba0d40699e70
matt_g <mgaut@uci.edu>
2018-09-22 14:24:25 -0700

    Created DamageTargetBonusEffectOnKill effect

---
+++ Assets/Scripts/Effects/Targeted/DamageTargetBonusEffectOnKill.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTargetBonusEffectOnKill : TargetedEffect {
+
+    [SerializeField] int damage;
+    [SerializeField] UntargetedEffect bonus_effect;
+
+    protected override void Resolve(IEntity source, IEntity target) {
+        ICombatant comb = target as ICombatant;
+        if (comb == null) {
+            return;
+        }
+
+        source.DealDamage(comb, damage);
+
+        if (comb.dead) {
+            bonus_effect.Resolve(source);
+        }
+    }
+}
